549b1f5db9fda519f3250fe289b80c547b5cf96f
Paul Chen <paulchen647@gmail.com>
2020-10-14 19:29:47 +0000

    Fix typos in migration. Download provided seeds.

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE properties(
   cost_per_night INTEGER NOT NULL DEFAULT 0,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  nubmer_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE property_reviews(
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  resevation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT 
 );
